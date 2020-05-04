11024f00ca842bab32be3ffec99f777704fd34d3
root <root@7df0796029bb>
2020-05-04 20:29:32 +0000

    Started houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,28 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot rob = new Robot();
 	public void run() {
-		
+		rob.setSpeed(100);
+		rob.setX(50);
+		rob.setY(300);
+		for(int i = 0; i<10; i++){
+	drawHouse(33);
+		}
 	}
+	public void drawHouse (String a) {
+		if (a == "small")
+		rob.penDown();
+		rob.move(a);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(a);
+		rob.turn(270);
+		rob.move(40);
+		rob.turn(270);
+	}
+	
 }
